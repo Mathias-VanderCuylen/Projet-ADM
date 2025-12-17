889005e75ff6618fc6eec850d51e751d28f7eeb6
Mathias-VanderCuylen <mathiasvandercuylen@gmail.com>
2025-12-17 11:01:19 +0100

    Fix: getSuite usage

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
         Sequence sequence = new Sequence();
         if (!sequence.isValid()) {
             System.out.println("Le théoreme de Hull-Dobell n'est pas vérifié.");
-            System.out.println(sequence);
+            System.out.println(sequence.getSuite());
         } else {
-            System.out.println(sequence);
+            System.out.println(sequence.getSuite());
             FrequencyTest.run(sequence.getSuite());
             PokerTest.run(sequence.getSuite(), sequence.getM());
         }
 
-//        Da da = new Da();
-//        System.out.println("Nombre de stations optimal : " + da.nbStationsOptimal(20));
+        Da da = new Da();
+        System.out.println("Nombre de stations optimal : " + da.nbStationsOptimal(20));
     }
 }
